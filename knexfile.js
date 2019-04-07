aec50b970fd0626a59100ecd59def472ce9a0e8a
michaelsnow3 <mikesnow444@gmail.com>
2019-04-07 19:38:15 -0400

    fixed syntax error

--- knexfile.js
+++ knexfile.js
@@ -22,7 +22,7 @@ module.exports = {
   production: {
     client: "postgresql",
     connection: {
-      url: DATABASE_URL,
+      url: process.env.DATABASE_URL,
       host: process.env.DB_HOST,
       user: process.env.DB_USER,
       password: process.env.DB_PASS,
